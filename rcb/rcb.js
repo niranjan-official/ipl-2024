3bb17a70bc12fa0da8a4d1576580ad415bd13488
christyjohnbiju <christyjb2003@gmail.com>
2024-04-07 13:19:10 +0530

    rcb added

---
+++ rcb/rcb.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    var loadingScreen = document.querySelector('.loading-screen');
+    var mainContent = document.querySelector('.main-content');
+    loadingScreen.style.display = 'none';
+    mainContent.style.display = 'block';
+  }, 4000);
+
+
